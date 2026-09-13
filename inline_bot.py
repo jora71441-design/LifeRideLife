18899b60b4b152141ecdb12705ae96dc40c4362e
jora71441-design <jora71441@gmail.com>
2026-09-13 12:47:22 +0300

    Update inline_bot.py

--- inline_bot.py
+++ inline_bot.py
@@ -79,7 +79,7 @@ async def inline_query_handler(query: types.InlineQuery):
                     reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(
         text=nav["btn_text"], 
-        url=f"https://jora71441-design.github.io/LifeRideLife/?cat={nav['param']}"
+        url=f"https://t.me/liferidelife_bot?startapp={nav['param']}"
     )
 ]])
 
